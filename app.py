de938453d8f92b8d82cd4f076f9df3ce9b67a4e0
mostafatahsin <mostafatahsin7@gmail.com>
2020-04-12 07:35:26 +0600

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from bokeh.plotting import figure, output_notebook, show, save
 
 from bokeh.models.tools import HoverTool
 from bokeh.transform import cumsum
-from bokeh.models import Text
+from bokeh.models import Text,Label
 from flask import render_template
 from bokeh.embed import components
 
@@ -13,27 +13,10 @@ import pandas as pd
 import math
 
 app = Flask(__name__, template_folder='templates')
-# 5000
+
 def make_plot():
 
     pi = math.pi
-
-    # x=[1,2,3,4]
-    # y=[4,5,7,5]
-
-    # x = {
-    #     'Telemedicine': 200,
-    #     'Education':    200,
-    #     'Transportation':   200,
-    #     'Banking':  200,
-    #     'Hospitals':    200,
-    #     'Domestic': 500,
-    #     'Exports':  500,
-    #     'Freelance':    1000
-    # }
-
-
-    # data = pd.Series(x).reset_index(name='value').rename(columns={'index':'country'})
 
     data = pd.DataFrame()
     list1=['Telemedicine','Education','Transportation','Banking','Hospitals','Domestic','Exports','Freelance']
@@ -41,31 +24,49 @@ def make_plot():
     data['country'] = list1
     data['value'] =list2
 
-
     data['angle'] = data['value']/data['value'].sum() * 2*pi
     data['color'] = ['#084594', '#2171b5', '#4292c6', '#6baed6', '#9ecae1', '#fbbb3c','#f0901a','#de5050']
     data['skill_1'] = ["Full-stack developers","IT courses-instructors","Full-stack developers","Hardware and Network system skills","Java, Android and IOS development","Web Application","Web Application",'Software development']
-    data['skill_2'] = ['Mobile, web & cloud-based application development', 'IT-enabling of administrative services', 'Software quality assurance, Testing and Technical support', 'MS Office Suite', 'Linux, Windows and Virtualization (in particular, ESXi)40', 'Web Design', 'Mobile Application', 'Web design and development' ]
-    data['skill_3'] = ['Database administration', 'IT-enabling of accounting services', 'network-related activities-CISCO/Microsoft certified applicants', 'Accessing networks/servers', 'Routing, Switching for Networking', 'Mobile Application','ERP development', 'System architecture' ]
+    data['skill_2'] = ['Mobile, Web & Cloud-based App development', 'IT-enabling of administrative services', 'Software Quality Assurance, Testing and Technical support', 'MS Office Suite', 'Linux, Windows and Virtualization (ESXi)', 'Web Design', 'Mobile Application', 'Web design and development' ]
+    data['skill_3'] = ['Database administration', 'IT-enabling of accounting services', 'CISCO/Microsoft certification', 'Accessing networks/servers', 'Routing, Switching for Networking', 'Mobile Application','ERP development', 'System architecture' ]
     data['skill_4'] = ['Data analytics', 'Data entry', 'MS office', 'Java and C++', 'V-LAN for Networking', 'Hardware, Server Installation and Maintenance','QA Testing', 'Graphic design']
-    data['skill_5'] = ['Problem-solving mindset and adaptability', 'Administrative skills', 'Communication and presentation skills', 'Hardware, software and network troubleshooting', 'Basic computer literacy for staff', 'ERP development ','Web Design', 'Java & Html related product/services ']
-    # data = data.sort_values('value')
+    data['skill_5'] = ['Problem-solving mindset and adaptability', 'Administrative skills', 'Communication and presentation skills', 'Hardware, Software and Network troubleshooting', 'Basic computer literacy', 'ERP development ','Web Design', 'HTML & JAVA programming language']
 
-    
     # Add plot
-    p = figure(plot_height=500, plot_width=500, title="Top Five IT Industry Skills",
-               tools="pan, zoom_in,zoom_out,save,reset", x_range = (-0.5,1))
+    p = figure(plot_height=500, plot_width=700,
+                tools="pan,zoom_in,zoom_out,save,reset", x_range = (-0.5,1))
 
-    p.wedge(x=0, y=1, radius=0.35, 
+    p.wedge(x=0.15, y=1, radius=0.35, 
             start_angle=cumsum('angle', include_zero=True), end_angle=cumsum('angle'),
             line_color="white", fill_color='color', legend_field='country', source=data)
 
-    
+    #text=['FE firms', 'BE firms','freelancers']
+
+    citation1 = Label(x=0.3, y=1.54, x_units='data', y_units='data',
+                     text='Front End firms', render_mode='css',
+                     border_line_color='black', border_line_alpha=1.0,
+                     background_fill_color='#6baed6', background_fill_alpha=1.0)
+
+    citation2 = Label(x=-0.4, y=0.95, x_units='data', y_units='data',
+                     text='Back End firms', render_mode='css',
+                     border_line_color='black', border_line_alpha=1.0,
+                     background_fill_color='#fbbb3c', background_fill_alpha=1.0)
+
+    # citation3 = Label(x=0.2, y=0.65, x_units='data', y_units='data',
+    #                  text='Freelance', render_mode='css',
+    #                  border_line_color='black', border_line_alpha=1.0,
+    #                  background_fill_color='#ef6565', background_fill_alpha=1.0)
+
+    p.add_layout(citation1)
+    p.add_layout(citation2)
+    # p.add_layout(citation3)
+
+
     p.axis.axis_label=None
     p.axis.visible=None
     p.grid.grid_line_color = None
 
-    
+
 
     # Add Tooltips
     hover = HoverTool()
@@ -79,11 +80,7 @@ def make_plot():
         <div>5. @skill_5</div>
       </div>
     """
-
     p.add_tools(hover)
-
-    #show(p)
-    #output_notebook()
 
     script, div = components(p)
     return script, div
